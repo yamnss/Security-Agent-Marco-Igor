cad4dd6dddb06f787f618db1ae0a0e369938c967
yamnss <167879313+yamnss@users.noreply.github.com>
2026-03-15 17:15:06 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,61 @@
-git init
-git add edr_agent.py
-git commit -m "primeira versão do agente EDR"
-git branch -M main
-git remote add origin https://github.com/yamnss/Security-Agent-Marco-Igor/blob/main/edr.agent.py
-git push -u origin main
+#!/bin/bash
+
+echo "================================="
+echo " Instalando Linux EDR Agent"
+echo "================================="
+
+# atualizar pacotes
+
+sudo apt update -y
+
+# instalar dependências
+
+sudo apt install python3 python3-pip ufw curl -y
+
+# instalar biblioteca python
+
+pip3 install psutil
+
+# criar diretório do agente
+
+sudo mkdir -p /opt/edr-agent
+
+# baixar agente do github
+
+sudo curl -o /opt/edr-agent/edr_agent.py https://raw.githubusercontent.com/yamnss/Security-Agent-Marco-Igor/refs/heads/main/edr.agent.py?token=GHSAT0AAAAAADX4KNECJ5OTH2T7VM6VMTIK2NXCJPA
+
+# dar permissão de execução
+
+sudo chmod +x /opt/edr-agent/edr_agent.py
+
+# criar arquivo de log
+
+sudo touch /var/log/edr_agent.log
+sudo chmod 666 /var/log/edr_agent.log
+
+# criar serviço systemd
+
+sudo bash -c 'cat << EOF > /etc/systemd/system/edr-agent.service
+[Unit]
+Description=Linux EDR Agent
+After=network.target
+
+[Service]
+ExecStart=/usr/bin/python3 /opt/edr-agent/edr_agent.py
+Restart=always
+User=root
+
+[Install]
+WantedBy=multi-user.target
+EOF'
+
+# ativar serviço
+
+sudo systemctl daemon-reload
+sudo systemctl enable edr-agent
+sudo systemctl start edr-agent
+
+echo "================================="
+echo "Instalação concluída!"
+echo "EDR ativo no sistema."
+echo "================================="
