f9287ac2611efc450b345c1ef3faca27784f0bb3
yamnss <167879313+yamnss@users.noreply.github.com>
2026-03-15 17:07:23 -0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+git init
+git add edr_agent.py
+git commit -m "primeira versão do agente EDR"
+git branch -M main
+git remote add origin https://github.com/yamnss/Security-Agent-Marco-Igor/blob/main/edr.agent.py
+git push -u origin main
